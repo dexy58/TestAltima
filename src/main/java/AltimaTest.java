b080d173e8c6ff6a2244ec378562541bde148658
5ivek <ivan.kralj@fer.hr>
2018-11-21 19:47:30 +0100

    Read file ChildParent.txt

--- src/main/java/AltimaTest.java
+++ src/main/java/AltimaTest.java
@@ -1,5 +1,12 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 public class AltimaTest {
     public static void main(String[] args) {
-        
+        try {
+            FileInputStream fstream = new FileInputStream("C:\\ChildParent.txt");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
