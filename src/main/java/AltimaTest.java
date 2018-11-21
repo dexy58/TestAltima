5da7346f1b07e596e894dd54c25a75d4bd966b6a
5ivek <ivan.kralj@fer.hr>
2018-11-21 20:41:02 +0100

    Parse parent and names child and output the names (in beta)

--- src/main/java/AltimaTest.java
+++ src/main/java/AltimaTest.java
@@ -3,21 +3,39 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStreamReader;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 
 public class AltimaTest {
     public static void main(String[] args) {
-        Map<String, String> familyTree = new HashMap<>();
+        List<String> parentNames = new ArrayList<>();
+        List<String> childNames = new ArrayList<>();
         try {
             FileInputStream fstream = new FileInputStream("C:\\ChildParent.txt");
             BufferedReader br = new BufferedReader(new InputStreamReader(fstream));
             String strLine;
-            String[] namesList = new String[2];
+            String[] namesList;
             while((strLine=br.readLine()) != null) {
                 namesList = strLine.split(" ");
-                familyTree.put(namesList[1], namesList[0]);
+                childNames.add(namesList[0]);
+                parentNames.add(namesList[1]);
             }
+            int counter = 1;
+            while(true){
+                for(int i=0;i<parentNames.size();i++){
+                    for(int j=0;j<childNames.size();j++){
+                        if(parentNames.get(i).equals(childNames.get(j))){
+                            System.out.print("\t" + " parent");
+                        }
+                    }
+                    System.out.println(parentNames.get(i));
+                    for(int j=0;j<childNames.size();j++){
+
+                    }
+                }
+                break;
+            }
+
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e){
